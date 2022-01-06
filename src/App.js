69770e5cf5d3c914e1978655f6d8cb60938f514e
Joe Sentosa <58586283+joesentosa@users.noreply.github.com>
2022-01-06 13:42:49 +0700

    Forgot Password
    Tampilan forgot password

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Profile from 'Components/Page/Profile';
 import Chat_room from 'Components/Page/Chat-room';
 import PrivacyPolicy from 'Components/Page/PrivacyPolicy';
 import PageNotFound from 'Components/Page/PageNotFound';
+import ForgotPassword from 'Components/Page/ForgotPassword';
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -28,6 +29,7 @@ function App() {
           {/* <Route exact path={"/search"} element={<Search/>}/>
                 <Route exact path={"/post"} element={<Post/>}/>*/}
           <Route exact path={'/chat-room'} element={<Chat_room />} />
+          <Route exact path={'/forgot-password'} element={<ForgotPassword />} />
           {/* jika tidak ada url yang cocok dengan route" diatas*/}
           <Route element={<PageNotFound />} />
         </Routes>
